69580139828d5fc91377b5392c7b991de9c75477
charliemenor81 <charliemenor81@gmail.com>
2024-05-25 18:28:39 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ const btnlLike1 = document.getElementById("btnLike1")
 const countLike1 = document.getElementById("countLike1")
 function clickLike1(){
   let totalLikès = countLike1.value + 1
-  countLike1.textcontent = totalLikes.toString()
+  countLike1.textContent = totalLikes.toString()
 }
+btnLike1.addventListener("click",clickLike1)
