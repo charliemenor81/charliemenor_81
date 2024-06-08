d182a1e251b86b4bb4e1cef6e67a739f3dddc460
charliemenor81 <charliemenor81@gmail.com>
2024-06-08 18:32:27 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,41 @@
-const btnLike1 = document.getElementById("btnLike1")
-const countLike1 = document.getElementById("countLike1")
+const btnLike1 = document.getElementById("btnLike1");
+const btnDislike1 = document.getElementById("btnDislike1");
+const countLike1 = document.getElementById("countLike1");
+
+const btnLike2 = document.getElementById("btnLike2");
+const btnDislike2 = document.getElementById("btnDislike2");
+const countLike2 = document.getElementById("countLike2");
 
 function clickLike1(){
-  let totalLikes = parseInt(countLike1.value) + 1
-  countLike1.textContent = totalLikes.toString()
+  let totalLikes = parseInt(countLike1.textContent) + 1;
+  countLike1.textContent = totalLikes.toString();
 }
-btnLike1.addEventListener("click",clickLike1)
+btnLike1.addEventListener("click", clickLike1);
+
+function clickDislike1(){
+  let totalLikes = parseInt(countLike1.textContent) - 1;
+  countLike1.textContent = totalLikes.toString();
+}
+btnDislike1.addEventListener("click", clickDislike1);
 
 function clickLike2(){
-  let totalLikes = parseInt(countLike2.value) + 1
-  countLike2.textContent = totalLikes.toString()
+  let totalLikes = parseInt(countLike2.textContent) + 1;
+  countLike2.textContent = totalLikes.toString();
 }
-btnLike2.addEventListener("click",clickLike2)
+btnLike2.addEventListener("click", clickLike2);
 
-const comment = document.getElementById("comment")
-const submit = document.getElementById("submit")
-const commentbox = document.getElementById("commentbox")
-function submitComment(){
-    commentbox.textContent += comment.value.toString() + "\n"
-    comment.value=""
+function clickDislike2(){
+  let totalLikes = parseInt(countLike2.textContent) - 1;
+  countLike2.textContent = totalLikes.toString();
 }
-submit.addEventListener("click",submitComment)
+btnDislike2.addEventListener("click", clickDislike2);
+
+const comment = document.getElementById("comment");
+const submit = document.getElementById("submit");
+const commentbox = document.getElementById("commentbox");
+
+function submitComment(){
+    commentbox.textContent += comment.value.toString() + "\n";
+    comment.value = "";
+}
+submit.addEventListener("click", submitComment);
